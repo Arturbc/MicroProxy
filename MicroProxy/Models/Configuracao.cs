2c0f78dddb8ec41ee901c0c53d7fb74cac6ff608
Artur Benevenuto Coelho <artur-bc.mkt@outlook.com>
2024-10-19 19:25:56 -0300

    Push inicial

---
+++ MicroProxy/Models/Configuracao.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MicroProxy.Models
+{
+    public partial class Configuracao
+    {
+        protected IConfigurationRoot ConfigurationRoot { get; set; } = null!;
+        public string? Ip { get; protected set; } = null!;
+        public string? Porta { get; protected set; } = null!;
+        public string UrlAlvo { get; protected set; }
+
+        public Configuracao()
+        {
+            var configurationBuilder0 = new ConfigurationBuilder();
+            var path0 = Path.GetFullPath("appsettings.json");
+
+            configurationBuilder0.AddJsonFile(path0, false);
+            ConfigurationRoot = configurationBuilder0.Build();
+
+            UrlAlvo = ConfigurationRoot.GetValue<string>("UrlAlvo")!.TrimEnd('/');
+            Ip = ConfigurationRoot.GetValue<string>("IP");
+
+            if ((Ip ?? "") != "")
+            {
+                Porta = PortaIpRegex().Match(Ip).Value.TrimStart(':');
+                Ip = PortaIpRegex().Replace(Ip, "");
+            }
+        }
+
+        [GeneratedRegex(@":\d{1,5}")]
+        private static partial Regex PortaIpRegex();
+    }
+}
---
+++ MicroProxy/Program.cs
@@ -0,0 +1,81 @@
+using MicroProxy.Models;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Primitives;
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+
+internal static class Program
+{
+    static CookieContainer cookieContainer = new();
+    private static void Main(string[] args)
+    {
+        Configuracao configuracao = new();
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+
+        app.UseHttpsRedirection();
+
+        app.Use(async (context, next) =>
+        {
+            await next.ProcessarRequisicao(context, configuracao);
+        });
+
+        app.Run();
+    }
+    private static async Task ProcessarRequisicao(this RequestDelegate next, HttpContext context, Configuracao configuracao)
+    {
+        HttpClientHandler clientHandler = new() { CookieContainer = cookieContainer };
+        var request = context.Request;
+        var headersReq = request.Headers;
+        string hostAlvo = new Uri(request.GetDisplayUrl()).Host;
+        HttpClient httpClient = new(clientHandler);
+        string body;
+        HttpRequestMessage requestMessage = new(HttpMethod.Parse(request.Method), $"{configuracao.UrlAlvo}{request.GetEncodedPathAndQuery()}");
+
+        foreach (var chave in headersReq.Keys)
+        {
+            var valor = !chave.Equals("Host", StringComparison.CurrentCultureIgnoreCase) ? headersReq[chave].ToArray<string>() : [hostAlvo];
+
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation(chave, valor);
+        };
+
+        if (request.Method != HttpMethods.Get)
+        {
+            request.EnableBuffering();
+            body = await new StreamReader(request.Body).ReadToEndAsync();
+            requestMessage.Content = new StringContent(body, Encoding.UTF8, MediaTypeNames.Application.Json);
+        }
+
+        var response = await httpClient.SendAsync(requestMessage);
+        var headersResposta = response.Headers.ToDictionary();
+
+        foreach (var chave in headersResposta.Keys.Where(k => !k.Equals("Transfer-Encoding", StringComparison.CurrentCultureIgnoreCase)))
+        {
+            StringValues valores = new(headersResposta[chave].ToArray());
+
+            if (!context.Response.Headers.TryAdd(chave, valores))
+            {
+                context.Response.Headers.Append(chave, valores);
+            }
+        };
+
+        await next(context);
+        context.Response.StatusCode = (int)response.StatusCode;
+
+        if (context.Response.StatusCode < 300 || context.Response.StatusCode >= 400)
+        {
+            var conteudo = await response.Content.ReadAsByteArrayAsync();
+
+            context.Response.OnStarting(() => context.Response.BodyWriter.WriteAsync(conteudo).AsTask());
+            await context.Response.StartAsync();
+        }
+
+        await context.Response.CompleteAsync();
+    }
+}
