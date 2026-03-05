cfcd58c6e84a167aa032f8e3bf86e100a753c0f1
ARTUR COELHO <artur.coelho@mpm.mp.br>
2026-03-05 14:53:22 -0300

    Ocultação de arquivo inútil, padrão do projeto OpenAPI, e correção da lógica na validação inicial da conexão, que estava incorreta.

--- MicroProxy/Program.cs
+++ MicroProxy/Program.cs
@@ -88,7 +88,7 @@ foreach (var (listener, certificado) in tcpListeners)
         {
             var client = await listener.AcceptTcpClientAsync(app.Lifetime.ApplicationStopping);
             var clientStream = client.GetStream();
-            if (!clientStream.Socket.Poll(1000, SelectMode.SelectRead) || !clientStream.DataAvailable) { clientStream.Socket.Dispose(); continue; }
+            if (clientStream.Socket.Poll(1000, SelectMode.SelectRead) && !clientStream.DataAvailable) { clientStream.Socket.Dispose(); continue; }
             try { configuracao = new(); } catch { }
             if (configuracao.BufferReq > 0) { clientStream.Socket.ReceiveBufferSize = configuracao.BufferReq; }
             if (configuracao.BufferResp > 0) { clientStream.Socket.SendBufferSize = configuracao.BufferResp; }
