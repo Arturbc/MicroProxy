341b3c75d1c80d0aed6f6b148652a126af7d6baa
ARTUR COELHO <artur.coelho@mpm.mp.br>
2026-05-25 14:43:43 -0300

    Reordenação de carregamento do certificado antes dos IPs e adição de opção de criptografia.

--- MicroProxy/Models/Configuracao.cs
+++ MicroProxy/Models/Configuracao.cs
@@ -1,4 +1,5 @@
 ﻿using MicroProxy.DTOs;
+using MicroProxy.Helpers;
 using Microsoft.Extensions.Hosting.WindowsServices;
 
 namespace MicroProxy.Models
@@ -10,6 +11,7 @@ namespace MicroProxy.Models
         public const string COOKIE_SITE = "cookieSite";
         public const string PATH_SITE_ATUAL = "pathSiteAtual";
         public const string PATH_SITE_ORIGEM_REDIRECT = "pathSiteOrigemRedirect";
+        private static bool AvisosExibidos = false;
         private ConexaoEscutaDTO[]? _conexoesEscuta;
         protected IConfigurationRoot ConfigurationRoot { get; set; } = null!;
         public Dictionary<string, Log>? Logs { get; protected set; }
@@ -94,16 +96,30 @@ namespace MicroProxy.Models
 
             Logs = ConfigurationRoot.GetSection(nameof(Logs)).Get<Dictionary<string, Log>>();
             MinutosValidadeCookie = ConfigurationRoot.GetValue<uint>(nameof(MinutosValidadeCookie));
-            ConexoesEscuta = ConfigurationRoot.GetSection(nameof(ConexoesEscuta)).Get<ConexaoEscutaDTO[]>() ?? [];
-            IPs = ConfigurationRoot.GetSection(nameof(IPs)).Get<string[]>() ?? [];
-            IP = ConfigurationRoot.GetValue<string>(nameof(IP));
-            IpsBloqueados = ConfigurationRoot.GetSection(nameof(IpsBloqueados)).Get<string[]>() ?? [];
-            PortaHttp = ConfigurationRoot.GetValue<ushort?>(nameof(PortaHttp)) ?? 0;
-            RedirectPortaHttp = ConfigurationRoot.GetValue<bool>(nameof(RedirectPortaHttp));
             SolicitarCertificadoCliente = ConfigurationRoot.GetValue<bool>(nameof(SolicitarCertificadoCliente));
             CertificadoPrivado = ConfigurationRoot.GetValue<string>(nameof(CertificadoPrivado))?.Trim();
             CertificadoPrivadoChave = ConfigurationRoot.GetValue<string>(nameof(CertificadoPrivadoChave))?.Trim();
             CertificadoPrivadoSenha = ConfigurationRoot.GetValue<string>(nameof(CertificadoPrivadoSenha));
+
+            if (CertificadoPrivado == "") { CertificadoPrivado = null; }
+            if (CertificadoPrivadoChave == "") { CertificadoPrivadoChave = null; }
+            if (CertificadoPrivadoSenha == "") { CertificadoPrivadoSenha = null; }
+            if (!string.IsNullOrEmpty(CertificadoPrivadoSenha))
+            {
+                var novosDados = FuncoesHelper.TratarDadosSensiveis(new Dictionary<string, string?>() { { nameof(CertificadoPrivadoSenha), CertificadoPrivadoSenha } },
+                    !AvisosExibidos ? "autenticação do certificado" : "");
+
+                if (!novosDados.TryGetValue(nameof(CertificadoPrivadoSenha), out var senhaDecifrada)) { senhaDecifrada = CertificadoPrivadoSenha; }
+
+                CertificadoPrivadoSenha = senhaDecifrada;
+            }
+
+            IPs = ConfigurationRoot.GetSection(nameof(IPs)).Get<string[]>() ?? [];
+            IP = ConfigurationRoot.GetValue<string>(nameof(IP));
+            ConexoesEscuta = ConfigurationRoot.GetSection(nameof(ConexoesEscuta)).Get<ConexaoEscutaDTO[]>() ?? [];
+            IpsBloqueados = ConfigurationRoot.GetSection(nameof(IpsBloqueados)).Get<string[]>() ?? [];
+            PortaHttp = ConfigurationRoot.GetValue<ushort?>(nameof(PortaHttp)) ?? 0;
+            RedirectPortaHttp = ConfigurationRoot.GetValue<bool>(nameof(RedirectPortaHttp));
             ArquivosEstaticos = ConfigurationRoot.GetValue<string>(nameof(ArquivosEstaticos));
             CompressionResponse = ConfigurationRoot.GetValue<string>(nameof(CompressionResponse));
             TratamentoErroInterno = ConfigurationRoot.GetValue<string>(nameof(TratamentoErroInterno));
@@ -119,8 +135,8 @@ namespace MicroProxy.Models
             if (AllowOrigins.Length == 0) AllowOrigins = ["*"];
             if (AllowHeaders.Length == 0) AllowHeaders = ["*"];
             if (AllowMethods.Length == 0) AllowMethods = ["*"];
-            if (CertificadoPrivadoChave == "") CertificadoPrivadoChave = null;
-            if (CertificadoPrivadoSenha == "") CertificadoPrivadoSenha = null;
+
+            AvisosExibidos = true;
         }
     }
 }
